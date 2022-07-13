98fe14bfa91a5137319e3082472af1d55f97847c
bjankiewicz <bjankiewicz@google.com>
2022-07-13 07:32:48 +0200

    Move trigger to a dedicated file

--- airflow/providers/google/cloud/operators/dataproc.py
+++ airflow/providers/google/cloud/operators/dataproc.py
@@ -18,7 +18,6 @@
 #
 """This module contains Google Dataproc operators."""
 
-import asyncio
 import inspect
 import ntpath
 import os
@@ -1117,42 +1116,6 @@ class DataprocJobBaseOperator(BaseOperator):
         """
         if self.dataproc_job_id:
             self.hook.cancel_job(project_id=self.project_id, job_id=self.dataproc_job_id, region=self.region)
-
-
-class DataprocBaseTrigger(BaseTrigger):
-    """
-    TODO: description
-    """
-
-    def __init__(self, job_id, project_id, region, gcp_conn_id, impersonation_chain):
-        super().__init__()
-        self.gcp_conn_id = gcp_conn_id
-        self.impersonation_chain = impersonation_chain
-        self.job_id = job_id
-        self.project_id = project_id
-        self.region = region
-        self.hook = DataprocHook(
-            gcp_conn_id=self.gcp_conn_id, impersonation_chain=self.impersonation_chain, async_client=True
-        )
-
-    def serialize(self):
-        return (
-            "airflow.providers.google.cloud.operators.dataproc.DataprocBaseTrigger",
-            {"job_id": self.job_id, "project_id": self.project_id, "region": self.region},
-        )
-
-    def get_job(self) -> Job:
-        return self.hook.get_job(job_id=self.job_id, project_id=self.project_id, region=self.region)
-
-    def job_finished(self) -> bool:
-        job = self.get_job()
-        state = job.status.state
-        return state in (JobStatus.State.ERROR, JobStatus.State.DONE, JobStatus.State.CANCELLED)
-
-    async def run(self):
-        while self.job_finished():
-            await asyncio.sleep(10)
-        yield TriggerEvent({"job": self.get_job()})
 
 
 class DataprocSubmitPigJobOperator(DataprocJobBaseOperator):
