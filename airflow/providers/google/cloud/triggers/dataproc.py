ba0c4ba0aa76efd107467d3e1d45e3ee996e3ce9
bjankiewicz <bjankiewicz@google.com>
2022-07-19 20:46:59 +0200

    test

--- airflow/providers/google/cloud/triggers/dataproc.py
+++ airflow/providers/google/cloud/triggers/dataproc.py
@@ -59,16 +59,4 @@ class DataprocBaseTrigger(BaseTrigger):
             job = self.get_job(project_id=self.project_id, region=self.region, job_id=self.job_id)
             self.log("Job status: %s", job)
             break
-            # state = job.status.state
-            # self.log.info("Job %s state is: %s", self.job_id, state)
-            # if state in (JobStatus.State.ERROR, JobStatus.State.DONE, JobStatus.State.CANCELLED):
-            #     if state in (JobStatus.State.DONE, JobStatus.State.CANCELLED):
-            #         break
-            #     elif state == JobStatus.State.ERROR:
-            #         raise AirflowException(f"Dataproc job execution failed {self.job_id}")
-            #     else:
-            #         raise AirflowException(
-            #             f"Dataproc job execution finished in uknknown state {state} {self.job_id}"
-            #         )
-            # await asyncio.sleep(self.pooling_period_seconds)
         yield TriggerEvent({"job": job})
